dc66c5efe0092dfc5b308c89cf570c413780e5f3
Rahul A Mistry <rahulmistry.sde@gmail.com>
2025-09-01 12:46:18 +0530

    feat: add hero section with gradient text and animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { HeroSection } from "./components/hero-section"
+
 export default function Home() {
   return (
-    <div className="min-h-[calc(100vh-136px)] lg:min-h-[calc(100vh-188px)] flex items-center justify-center">
-      
+    <div>
+      <HeroSection />
     </div>
   )
 }
